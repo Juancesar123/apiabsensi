2c7580aa52a0fe21804d960af87873ed3032b6fc
juancesarandrianto <juancesarandrianto@gmail.com>
2017-09-10 00:37:55 +0700

    added status in data users

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -35,6 +35,10 @@ module.exports = function (app) {
       type: Sequelize.STRING,
       allowNull: true
     },
+    status: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
   
   
   }, {
